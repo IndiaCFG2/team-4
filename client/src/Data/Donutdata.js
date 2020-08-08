d6fc73cd936ba219ec1dca4114e7c3b96516663e
nithya <nithyachivunuri@gmail.com>
2020-08-09 00:30:04 +0530

    Pie Chart feature

---
+++ client/src/Data/Donutdata.js
@@ -0,0 +1,19 @@
+ 
+ const Donutdata1={
+    labels: ['Agree','Disagree','Neutral opinion'],
+    datasets:[{
+        data: [100,40,10],
+        backgroundColor:['green','red','blue']
+    }]
+}
+const Donutdata2={
+    labels: ['Agree','Disagree','Neutral opinion'],
+    datasets:[{
+        data: [50,60,5],
+        backgroundColor:['green','red','blue']
+    }]
+}
+
+
+
+export {Donutdata1,Donutdata2};
