ceb5274dc5ad42da8b00afaffcca6d46360aaeda
ymeghana <ymeghana2464@gmail.com>
2020-08-08 18:08:41 +0530

    created react app

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div> <h1>Hello</h1> </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+//const db = require('./config/mongoDBKey').mongoURI;
+
+/* mongoose
+    .connect(db)
+    .then( () => console.log('MongoDB Connection Established.') )
+    .catch( err => console.log(err)); */
+
+app.get('/', (req, res) => {
+    res.json({status: 'Available'}) 
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server runnng on port ${port}`)
+}); 
