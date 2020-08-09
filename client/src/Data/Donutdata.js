1339a2df62f65ddd499581491894306135e8d339
Meghana Yelamarthi <33190366+ymeghana@users.noreply.github.com>
2020-08-09 07:20:33 +0530

    Updated with json

--- client/src/Data/Donutdata.js
+++ client/src/Data/Donutdata.js
@@ -1,15 +1,17 @@
- 
- const Donutdata1={
+import data from './data.json'
+const senti = data.sentimentAnalyis
+const quest = data.objQue.Que
+const Donutdata1={
     labels: ['Agree','Disagree','Neutral opinion'],
     datasets:[{
-        data: [100,40,10],
+        data: [senti.agree,senti.disagree,senti.neutral],
         backgroundColor:['green','red','blue']
     }]
 }
 const Donutdata2={
-    labels: ['Agree','Disagree','Neutral opinion'],
+    labels: ['Option-1','Option-2','Option-3'],
     datasets:[{
-        data: [50,60,5],
+        data: [quest.option1.count,quest.option2.count,quest.option3.count],
         backgroundColor:['green','red','blue']
     }]
 }
